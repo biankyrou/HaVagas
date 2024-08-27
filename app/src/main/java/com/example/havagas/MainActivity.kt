561533394e5b041d1eff311d9d3b23fbaf1cb1e0
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-08-26 21:28:42 -0300

    feat and refactor: add phone save button and add EditText phone

--- app/src/main/java/com/example/havagas/MainActivity.kt
+++ app/src/main/java/com/example/havagas/MainActivity.kt
@@ -5,11 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+
+        var telefoneTipo = ""
+        var sexo = ""
+
+        amb.salvarBt.setOnClickListener {
+            telefoneTipo = if (amb.residencialRb.isChecked) {
+                "Residencial"
+            } else {
+                "Comercial"
+            }
+            sexo = if (amb.femininoRb.isChecked) {
+                "Feminino"
+            } else {
+                "Masculino"
+            }
+        }
     }
 }
